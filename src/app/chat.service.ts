a99a7bf5afda10219f77e86fe24bac4d03c54545
Barillon Terry <ptitesailes@gmail.com>
2024-11-07 03:52:25 +0100

    Removed the used of ngrok as i failed to config it, will try again

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -9,7 +9,7 @@ export class ChatService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('https://427b-2a01-cb05-83c6-5100-438c-110-a640-e315.ngrok-free.app/');
+    this.socket = io('http://localhost:3000');
   }
 
   sendMessage(message: any): void {
