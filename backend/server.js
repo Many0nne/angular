d9634380639aaee4848e35f1b05d0ed087657dcd
Manyonne <ptitesailes@gmail.com>
2025-05-13 22:36:09 +0200

    problème de cors ?

--- backend/server.js
+++ backend/server.js
@@ -24,16 +24,21 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-const allowedOrigins = ['http://localhost:4200', 'https://angular-terry-barillon.vercel.app'];
+// const allowedOrigins = ['http://localhost:4200', 'https://angular-terry-barillon.vercel.app'];
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: '*',
   credentials: true
 }));
 
